84bf5755bad2e7f772d37a8b86cbd3984ea51930
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-25 16:40:25 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,12 +3,12 @@
     value = aws_eip.Docker_EIP.public_ip
 }*/
 
-output "DBZ_IP" {
-    description = "DBZ Terraform Instance Public IP"
-    value = aws_instance.DBZ.public_ip
+output "Docker_IP" {
+    description = "Docker Instance Public IP"
+    value = aws_instance.Docker.public_ip
 }
 
 output "DBZ_DNS" {
-    description = "DBZ Terraform Instance Public DNS"
-    value = aws_instance.DBZ.public_dns
+    description = "Docker Instance Public DNS"
+    value = aws_instance.Docker.public_dns
 }
