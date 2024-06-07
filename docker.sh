9c36fff6d690abb570841ffe9e5530a72688dd04
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-06-07 08:25:21 -0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@ sudo hostnamectl set-hostname docker
 sudo adduser docker
 sudo echo "docker ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/docker
 sudo su - docker
-sudo chown docker:docker -R /etc/ssh/sshd_config
-sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo chown docker:docker -R /etc/ssh/sshd_config.d
+sudo sed -i 's/no/yes/' /etc/ssh/sshd_config.d/*.conf
 sudo service sshd restart
 sudo apt update -y 
 sudo apt install openjdk-17-jdk -y
@@ -12,3 +12,8 @@ sudo apt update -y
 sudo apt install docker.io -y
 sudo usermod -aG docker docker 
 sudo systemctl restart docker.service
+
+
+#sudo chown docker:docker -R /etc/ssh/sshd_config
+#sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+
