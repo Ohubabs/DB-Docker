9e1f633a140cb8070ed858898a0f6dab9dce79ba
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-06-24 15:48:46 -0700

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "Docker" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = "terra"
-    subnet_id = var.public_subnet_id
+    subnet_id = var.docker_public_subnet_id
     availability_zone = var.availability_zone
     associate_public_ip_address = true  
     vpc_security_group_ids = var.vpc_security_group_id
+    user_data = file("${path.module}/docker.sh")  
     root_block_device {
       volume_size = 10 
       encrypted  = true
@@ -13,12 +14,11 @@ resource "aws_instance" "Docker" {
       tags ={
         "Name" = "Docker_Volume"
       }
-    user_data = file("${path.module}/docker.sh")        
+    }       
     tags = {
-      "name" = "Docker" 
+      "Name" = "Docker" 
     }  
 }
-
 /*resource "aws_eip" "Docker_EIP" {
   instance = aws_instance.Docker.id
 }*/
